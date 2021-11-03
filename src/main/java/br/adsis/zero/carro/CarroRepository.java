c72a62e2f399c461208f17c6b651c20c815ac7c3
Julio-treichel <julio.tre@outlook.com>
2021-11-03 00:04:26 -0300

    first commit

---
+++ src/main/java/br/adsis/zero/carro/CarroRepository.java
@@ -0,0 +1,8 @@
+package br.adsis.zero.carro;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarroRepository extends JpaRepository<Carro, String> {
+
+	
+}
---
+++ src/main/java/br/adsis/zero/carro/CarroService.java
@@ -0,0 +1,21 @@
+package br.adsis.zero.carro;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class CarroService {
+	@Autowired
+	private CarroRepository repo;
+	
+	public List<Carro> obterTodos(){
+		return repo.findAll();
+	}
+	
+	
+}
